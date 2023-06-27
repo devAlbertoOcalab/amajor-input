dcb19f66a85b89f6dda9b1e451df6e0ff2a1839a
devAlbertoOcalab <dev.alberto@ocalab.it>
2023-06-27 17:58:49 +0200

    insert pin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'amajor-input';
+
+  inputArray: number[] = [];
+
+  // !!!!!!!!!!!!!!! se uso l'array "pin" tipizzato in questa maniera non riesco ad utilizzare la funzione includes a riga 41!!!!!!!!!!!!!!!!!!!!
+  // pin: number[] | null[] = [null, null, null, null, null, null, null, null];
+  pin: any = [null, null, null, null, null, null, null, null];
+
+  @HostListener('document:keydown', ['$event'])
+  onKeydownHandler(event: KeyboardEvent) {
+    event.preventDefault();
+
+    // controllo che input sia un numero o che l'array non superi la lunghezza prestabilita
+    if (Number.parseInt(event.key, 10) && this.inputArray.length < this.pin.length) {
+      this.inputArray.push(Number.parseInt(event.key, 10));
+    }
+    // se si preme backspace toglie ad inputArray l'ultimo valore e resetta in null il valore inserito in precedenzq
+    else if (event.key === 'Backspace') {
+      this.inputArray.pop();
+      this.pin[this.inputArray.length] = null;
+    }
+
+    // aggiungo alla sua posizione il valore inserito
+    if (this.inputArray.length > 0) {
+      this.inputArray.forEach((input: number, index: number) => {
+        this.pin[index] = input;
+      });
+    }
+  }
+
+  ableButton() {
+    // se include ancora dei null all'interno dell'array il bottone rimane disabilitato
+    return this.pin.includes(null);
+  }
 }
